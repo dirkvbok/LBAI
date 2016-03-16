6c3c409da31b8f903e11dec747366af977608a92
96Asch <aschhuang@gmail.com>
2016-03-16 15:51:35 +0100

    Update ex 5

--- robot.pl
+++ robot.pl
@@ -5,10 +5,12 @@
 	sequence/1,		% matches the sequence/1 percept
 	at/1,			% matches the at/1 percept
 	color/2,		% matches the color/2 percept
-	holding/1,		% matches the holding/1 percept		
+	holding/1,		% matches the holding/1 percept
 	atBlock/1,		% matches the atBlock/1 percept
-	sequenceIndex/1.	% matches the sequenceIndex/1 percept
-	
+	sequenceIndex/1,	% matches the sequenceIndex/1 percept
+	visited/1,		% matches the visited/1 percept
+	block/2.
+
 
 % A room is a place with exactly one neighbour, i.e., there is only one way to get to and from that place.
 room(PlaceID) :- zone(_,PlaceID,_,_,Neighbours), length(Neighbours,1).
