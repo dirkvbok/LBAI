b4d201ebcc640a190e9c5e597de4f1977ce3cd78
96Asch <aschhuang@gmail.com>
2016-04-01 18:31:33 +0200

    Added new adopt in('DropZone') + Comments

--- robot.pl
+++ robot.pl
@@ -15,7 +15,6 @@
 	returnBlock/1,
 	deliverBlock/1,
 	sentMessage/1,
-	pickedUp/1,
 	running/0.
 
 % A room is a place with exactly one neighbour, i.e., there is only one way to get to and from that place.
