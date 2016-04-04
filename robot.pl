d33435b6f099e73d08e80b6f6a28236fc4c9fbce
96Asch <aschhuang@gmail.com>
2016-04-04 17:41:03 +0200

    Final version

--- robot.pl
+++ robot.pl
@@ -15,9 +15,8 @@
 	returnBlock/1,
 	deliverBlock/1,
 	sentMessage/1,
-	running/0,
-	bring/1,
-	return/1.
+	brought/1,
+	terminated/0.
 
 % A room is a place with exactly one neighbour, i.e., there is only one way to get to and from that place.
 room(PlaceID)	 			:- zone(_,PlaceID,_,_,Neighbours), length(Neighbours,1).
